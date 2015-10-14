a021580e706a6fd9e75ef5f2b4d8d3c347bf0065
Jose Miguel Melo <melo199435@gmail.com>
2015-10-14 12:33:14 +0100

    Main website layout added

--- ecosense/config/routes.rb
+++ ecosense/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'users/new'
-
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
