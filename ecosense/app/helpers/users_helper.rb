a0081517793818bbef594f2d1ce5e4b50552f7fc
tiagommferreira <tiagommferreira55@gmail.com>
2015-10-29 14:50:51 +0000

    Update user avatar

--- ecosense/app/helpers/users_helper.rb
+++ ecosense/app/helpers/users_helper.rb
@@ -1,12 +1,5 @@
 module UsersHelper
 
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "about-portrait img-responsive")
-  end
-
   # Returns the User logo
   def get_logo(user)
     default_url = 'http://www.caxtoneditorial.co.uk/en/wp-content/uploads/2012/02/User.png'
--- ecosense/app/models/user.rb
+++ ecosense/app/models/user.rb
@@ -25,4 +25,7 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "http://www.caxtoneditorial.co.uk/en/wp-content/uploads/2012/02/User.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
