4859de41b8d15d92721c50c506dc83f6e13573cc
Jose Miguel Melo <melo199435@gmail.com>
2015-12-17 14:21:20 +0000

    api get user by token

--- ecosense/app/controllers/api_controller.rb
+++ ecosense/app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   before_action :doorkeeper_authorize!
+  skip_before_filter :verify_authenticity_token
 
   def posts
     posts = Post.all.reverse_order.eager_load(:user,:comments,:comments => :user)
@@ -32,16 +33,18 @@ class ApiController < ApplicationController
 
   def create
     post = Post.new(post_params)
-    post.user_id = params[:user_id]
+    post.user_id = current_resource_owner.id
     params[:company_id] != 0 ? post.company_id=params[:company_id] : post.company_id = 0
 
-    if Company.where(:id => params[:company_id]).blank?
+    if(post.company_id != 0)
+    if Company.where(:id => post.company_id).blank?
       render json: { success: false, msg: "Company not found." }
       return
     end
-    if Company.find(params[:company_id]).user_id != post.user_id
+    if Company.find(post.company_id).user_id != post.user_id
       render json: { success: false, msg: "Company given not valid." }
       return
+    end
     end
 
 
@@ -55,13 +58,13 @@ class ApiController < ApplicationController
   end
 
   def like
-    post = Post.find(params[:id])
-    like = Like.where(post_id: post.id, user_id: params[:user_id])
+    post = Post.find(current_resource_owner.id)
+    like = Like.where(post_id: post.id, user_id: current_resource_owner.id)
     if like.present?
       like.destroy_all
     else
       like = Like.new
-      like.user_id = params[:user_id]
+      like.user_id = current_resource_owner.id
       like.post_id = post.id
       like.save
     end
@@ -70,14 +73,14 @@ class ApiController < ApplicationController
 
   def share
     post = Post.find(params[:id])
-    share = Share.where(post_id: post.id, user_id: params[:user_id])
+    share = Share.where(post_id: post.id, user_id: current_resource_owner.id)
     message = ''
     if share.present?
       share.destroy_all
       message = 'removed shared post'
     else
       share = Share.new
-      share.user_id = params[:user_id]
+      share.user_id = current_resource_owner.id
       share.post_id = post.id
       share.save
       message = 'shared post successfully'
@@ -95,4 +98,10 @@ class ApiController < ApplicationController
     params.permit(:title, :teaser, :content, :latitude, :longitude, :ecological_issue, :avatar)
   end
 
+  private
+
+  # Find the user that owns the access token
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
